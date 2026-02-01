d7c03fd01ab45b3ca9681b02f8c6c876077fa972
KirillK <lirostin@gmail.com>
2026-02-01 12:29:43 +0300

    Complite task mypy.

---
+++ lesson-03-formatting-and-linting/task.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def calculate_precision(y_pred: np.ndarray, y_true: np.ndarray) -> float:
+    tp = np.sum((y_pred == 1) & (y_true == 1))
+    fp = np.sum((y_pred == 1) & (y_true == 0))
+
+    if tp + fp == 0:
+        return 0.0
+
+    precision = tp / (tp + fp)
+    return float(precision)
+
+y_true = np.array([1, 1, 1, 1, 0, 1])
+y_pred = np.array([1, 0, 1, 0, 1, 0])
+
+precision : float = calculate_precision(y_pred, y_true)
+print(f"Precision: {precision:.2f}")
